b65102d843dd3b2c9fa7fdd53c2661d2feb23af8
Jonathan Sledge <sledge35434@gotoltc.edu>
2013-03-17 09:26:49 -0500

    Add BuyTicket Model

---
+++ TicketPurchases/TicketPurchases/Models/BuyTickets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketPurchases.Models
+{
+    public class BuyTickets
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
