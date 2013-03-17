ec742e1f3fc156ba904a599aebe46700f06c0d68
Jonathan Sledge <sledge35434@gotoltc.edu>
2013-03-17 10:14:10 -0500

    Clean up validation

--- TicketPurchases/TicketPurchases/Models/BuyTickets.cs
+++ TicketPurchases/TicketPurchases/Models/BuyTickets.cs
@@ -11,6 +11,8 @@ namespace TicketPurchases.Models
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+",
+ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter your phone")]
         public string Phone { get; set; }
